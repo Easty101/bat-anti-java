e5b1447af37748ea737c2a1386af418a6e7639ac
Easty101 <106346360+Easty101@users.noreply.github.com>
2024-07-07 22:42:41 +0200

    Update js_service_diagnostics.bat

--- js_service_diagnostics.bat
+++ js_service_diagnostics.bat
@@ -1,18 +1,35 @@
 @echo off
+setlocal enabledelayedexpansion
+
 :loop
-curl -s -o nul -w "%{http_code}" http://pi-bot.local/trigger
-if %errorlevel%==0 (
-    echo Trigger aktiviert!
-	REM Hier kommt der Code, der ausgeführt werden soll
-	tasklist /fi "ImageName eq javaw.exe" /fo csv 2>NUL | find /I "javaw.exe">nul
-	if "%errorlevel%"=="0" (set "running=1") else (set "running=0")
-	if %running%==1 goto kill
-	timeout /t 120 /nobreak > nul
+echo start
+curl -s -o nul -w "%%{http_code}" http://pi-bot.local:8080/trigger > response.txt
+set /p response_code=<response.txt
+
+if !errorlevel! equ 0 (
+    if !response_code! equ 200 (
+        echo Trigger aktiviert!
+        REM Hier kommt der Code, der ausgeführt werden soll
+        tasklist /fi "ImageName eq javaw.exe" /fo csv 2>NUL | find /I "javaw.exe">nul
+        if !errorlevel! equ 0 (
+            set "running=1"
+        ) else (
+            set "running=0"
+        )
+        if !running! equ 1 goto kill
+		echo no kill
+        timeout /t 10 /nobreak > nul
+    ) else (
+		echo no Trigger
+	)
 )
-timeout /t 10
+
+timeout /t 10 /nobreak > nul
 goto loop
 
 :kill
+echo kill
 taskkill /f /im javaw.exe >nul
-timeout /t 300 /nobreak > nul
+timeout /t 5 /nobreak > nul
+echo timer done
 goto loop
