5887b1cb666fc6064bdf4cf83e6098f1245f64f2
Easty101 <106346360+Easty101@users.noreply.github.com>
2024-07-17 22:40:55 +0200

    Update update.bat

--- V2/update.bat
+++ V2/update.bat
@@ -1,6 +1,6 @@
 @echo off
 cd %temp%
-mkdir permanent
 cd permanent
 curl -O https://raw.githubusercontent.com/Easty101/bat-anti-java/main/runner.bat
-call runner.bat
+start C:\path\to\SilentCMD\SilentCMD.exe %temp%\permanent\runner.bat
+REM !Change 'C:\path\to\SilentCMD\SilentCMD.exe' to your actual path! ################################################################
