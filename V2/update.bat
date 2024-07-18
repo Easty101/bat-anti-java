b8b99c9826e2407234571d56f397fe1d8fe7dfa6
Easty101 <106346360+Easty101@users.noreply.github.com>
2024-07-18 13:15:08 +0200

    Update update.bat
    update.bat can now be used to create 'permanent' folder and download runner.bat

--- V2/update.bat
+++ V2/update.bat
@@ -1,5 +1,6 @@
 @echo off
 cd %temp%
+mkdir permanent
 cd permanent
 curl -O https://raw.githubusercontent.com/Easty101/bat-anti-java/main/V2/runner.bat
 start C:\path\to\SilentCMD\SilentCMD.exe %temp%\permanent\runner.bat
