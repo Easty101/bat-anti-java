bef85e2d438a9538d7e85d5c70a249ef5be8bed8
Easty101 <106346360+Easty101@users.noreply.github.com>
2024-07-28 15:39:26 +0200

    Update runner_old.bat

--- V2/runner_old.bat
+++ V2/runner_old.bat
@@ -1,23 +1,25 @@
 @echo off
+setlocal enabledelayedexpansion
+
 :loop
 REM Check Trigger 1
 for /f "delims=" %%i in ('curl -s -o nul -w "%%{http_code}" http://pi-bot.local:8080/trigger1') do set HTTP_CODE=%%i
+echo Trigger 1 HTTP_CODE: %HTTP_CODE%
 if "%HTTP_CODE%"=="200" (
-    curl -s http://pi-bot.local:8080/reset/trigger1
     call :do-something-1
 )
 
 REM Check Trigger 2
 for /f "delims=" %%i in ('curl -s -o nul -w "%%{http_code}" http://pi-bot.local:8080/trigger2') do set HTTP_CODE=%%i
+echo Trigger 2 HTTP_CODE: %HTTP_CODE%
 if "%HTTP_CODE%"=="200" (
-    curl -s http://pi-bot.local:8080/reset/trigger2
     call :do-something-2
 )
 
 REM Check Trigger 3
 for /f "delims=" %%i in ('curl -s -o nul -w "%%{http_code}" http://pi-bot.local:8080/trigger3') do set HTTP_CODE=%%i
+echo Trigger 3 HTTP_CODE: %HTTP_CODE%
 if "%HTTP_CODE%"=="200" (
-    curl -s http://pi-bot.local:8080/reset/trigger3
     call :do-something-3
 )
 
@@ -28,13 +30,13 @@ goto loop
 echo Aktion 1 ausgefuehrt!
 REM Hier kommt der Code, der fuer Trigger 1 ausgefuehrt werden soll
 tasklist /fi "ImageName eq javaw.exe" /fo csv 2>NUL | find /I "javaw.exe">nul
-if !errorlevel! equ 0 (
+if %ERRORLEVEL% equ 0 (
     set "java_running=1"
 ) else (
     set "java_running=0"
 )
-if !java_running! equ 1 (
-    call kill
+if %java_running% equ 1 (
+    call :kill
 ) else (
     echo no kill
     timeout /t 10 /nobreak > nul
@@ -51,16 +53,8 @@ goto :eof
 REM hopefully a self updating version using github ;D
 echo Aktion 3 ausgefuehrt!
 REM Hier kommt der Code, der fuer Trigger 3 ausgefuehrt werden soll
-call self.update
+call :self.update
 goto :eof
-
-REM ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-
-REM #######################
-REM ### Other functions ###
-REM #######################
-
-REM ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
 :self.update
 cd %temp%
@@ -69,6 +63,7 @@ cd permanent
 curl -O https://raw.githubusercontent.com/Easty101/bat-anti-java/main/V2/update.bat
 start C:\path\to\SilentCMD\SilentCMD.exe %temp%\permanent\update.bat
 REM !Change 'C:\path\to\SilentCMD\SilentCMD.exe' to your actual path! ################################################################
+goto :eof
 
 :kill
 echo kill
